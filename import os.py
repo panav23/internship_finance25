7a6b82531e025656daab20cc18f349a049d18653
panav23 <127978601+panav23@users.noreply.github.com>
2025-05-07 23:14:20 +0530

    Update import os.py

--- import os.py
+++ import os.py
@@ -1,45 +1,22 @@
+        
+from datetime import date, timedelta
+from jugaad_data.nse import bhavcopy_save
 import os
-import requests
-from datetime import datetime, timedelta
 
 
-DOWNLOAD_DIR = "nse_bhavcopies"
-os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+save_dir = "nse_bhavcopies"
+os.makedirs(save_dir, exist_ok=True)
 
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://www.nseindia.com"
-}
-
-
-start_date = datetime(2014, 1, 1)
-end_date = datetime.today()
-
-session = requests.Session()
-session.headers.update(HEADERS)
+start_date = date(2014, 1, 1)
+end_date = date.today()
 
 current_date = start_date
-
 while current_date <= end_date:
-    
-    if current_date.weekday() < 5:
-        day = current_date.strftime('%d')
-        month = current_date.strftime('%b').upper()
-        year = current_date.strftime('%Y')
-        date_str = current_date.strftime('%d%m%Y')
+    try:
         
-        filename = f"cm{date_str}bhav.csv.zip"
-        url = f"https://www1.nseindia.com/content/historical/EQUITIES/{year}/{month}/{filename}"
-        filepath = os.path.join(DOWNLOAD_DIR, filename)
-        
-        if not os.path.exists(filepath):
-            try:
-                print(f"Downloading: {filename}")
-                response = session.get(url, timeout=10)
-                if response.status_code == 200:
-                    with open(filepath, "wb") as f:
-                        f.write(response.content)
-            except Exception as e:
-                print(f"Error on {date_str}: {e}")
+        bhavcopy_save(current_date, save_dir)
+        print(f"Downloaded Bhavcopy for {current_date}")
+    except Exception as e:
+        print(f"Failed to download Bhavcopy for {current_date}: {e}")
     current_date += timedelta(days=1)
